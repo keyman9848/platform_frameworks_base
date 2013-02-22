792ec36b51043dcd0e57968251ad8d826f6eda73
Daniel Fages <dan@androvm.org>
2013-02-22 15:07:09 +0100

    VirtualBox seamless mouse don't need INPUT_DEVICE_CLASS_CURSOR

--- services/input/EventHub.cpp
+++ services/input/EventHub.cpp
@@ -1076,7 +1076,6 @@ status_t EventHub::openDeviceLocked(const char *devicePath) {
             && test_bit(ABS_X, device->absBitmask)
             && test_bit(ABS_Y, device->absBitmask)) {
         device->classes |= INPUT_DEVICE_CLASS_TOUCH;
-        device->classes |= INPUT_DEVICE_CLASS_CURSOR;
     }
 
     // See if this device is a joystick.
